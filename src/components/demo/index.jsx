26690dc48eae37d5db9f25deda1df9f3f25269e0
samueldelesque <hello@samueldelesque.me>
2016-06-14 18:13:30 -0400

    update viewport property

--- src/components/demo/index.jsx
+++ src/components/demo/index.jsx
@@ -2,8 +2,8 @@ module.exports = function(html, initialState, bundle, bodyClass){
   return `<!DOCTYPE html>
 <html>
   <head>
-    <title>SaladUI Components</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <title>Salad-UI Components</title>  
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
     <link rel="stylesheet" href="/demo/demo.css"/>
   </head>
   <body class="${bodyClass}">
