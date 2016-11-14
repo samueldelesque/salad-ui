1ecffb779ac13c96315adb3f06b068047c92655b
samueldelesque <hello@samueldelesque.me>
2016-11-14 12:37:43 -0500

    remvoe format-number component

--- src/components/util/util.jsx
+++ src/components/util/util.jsx
@@ -2,7 +2,6 @@ import Alert from './alert/alert'
 import Anchor from './anchor/anchor'
 import Badge from './badge/badge'
 import Button from './button/button'
-import FormatNumber from './format-number/format-number'
 import Grid from './grid/grid'
 import ProfilePicture from './profile-picture/profile-picture'
 import TagList from './tag-list/tag-list'
@@ -16,7 +15,6 @@ export default {
   Anchor,
   Badge,
   Button,
-  FormatNumber,
   Grid,
   TagList,
   TextClamp,
