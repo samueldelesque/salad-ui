00c4fa0772c34b00c7715ba5748ba469ae1e2d5c
samueldelesque <hello@samueldelesque.me>
2017-11-07 17:09:54 -0500

    Remove obsolete config

--- conf/entrypoints.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    'salad-ui': './src/components/salad-ui',
-    'salad-ui.lib': './src/lib/index',
-    'salad-ui.chart': './src/components/chart/index',
-    'salad-ui.form': './src/components/form/index',
-    'salad-ui.util': './src/components/util/index',
-}
